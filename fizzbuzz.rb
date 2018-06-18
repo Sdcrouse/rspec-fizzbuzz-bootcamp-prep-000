f0a5831c320a7a17ef4a7fa303bac1fa6e54c58f
Steven Crouse <sdcrouse91@gmail.com>
2018-06-18 03:13:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def fizzbuzz(int)
   # This also works for negative numbers!
-  binding.pry
+  
   if int % 15 == 0 # I could also say int % 3 == 0 && int % 5 == 0 
     int = "FizzBuzz"
   elsif int % 3 == 0 # I could put another if statement inside this (if int % 5 == 0)
@@ -9,5 +9,6 @@ def fizzbuzz(int)
   elsif int % 5 == 0 
     int = "Buzz"
   end
-  binding.pry
+  
+  int
 end
